6162f62c2a535c276c2efcadaa736a34c715e180
Sudhanshu Bhatt <sudhanshu.bhatt1988@gmail.com>
2015-07-27 18:32:11 -0400

    completed Challenges

--- app/src/main/java/com/example/sbhatt/geoquiz/controller/QuizActivity.java
+++ app/src/main/java/com/example/sbhatt/geoquiz/controller/QuizActivity.java
@@ -2,10 +2,12 @@ package com.example.sbhatt.geoquiz.controller;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,22 +23,28 @@ public class QuizActivity extends ActionBarActivity {
 
     private Button mTrueBtn;
     private Button mFalseBtn;
-    private Button mNextQtnBtn;
+    private ImageButton mNextQtnBtn;
+    private ImageButton mPrevQtnBtn;
     private TextView mQuestionView;
     private TrueFalse trueFalseObj;
     private ArrayList<TrueFalse> mQuestionBankList;
     private int mCurrentQuestionIndex = -1;
+    public static final String TAG = "QuizActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
+        // Logging that onCreate was clalled
+        Log.d(TAG,"onCreate(Bundle) was called");
+
         // Getting View Objects for buttons
         mTrueBtn = (Button)findViewById(R.id.trueBtn);
         mFalseBtn = (Button)findViewById(R.id.falseBtn);
-        mNextQtnBtn = (Button)findViewById(R.id.nextQuestnBtn);
+        mNextQtnBtn = (ImageButton)findViewById(R.id.nextQuestnBtn);
         mQuestionView = (TextView)findViewById(R.id.questionView);
+        mPrevQtnBtn = (ImageButton)findViewById(R.id.previousQuestnBtn);
 
         // Create Questions and add them to TrueFalse QuestionBank
         trueFalseObj = new TrueFalse();
@@ -50,7 +58,11 @@ public class QuizActivity extends ActionBarActivity {
         mTrueBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this,R.string.true_pressed_toast,Toast.LENGTH_SHORT).show();
+                boolean isRight = checkAnswer(true);
+                if(isRight)
+                    Toast.makeText(QuizActivity.this,R.string.true_pressed_toast,Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(QuizActivity.this,R.string.false_pressed_toast,Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -58,7 +70,11 @@ public class QuizActivity extends ActionBarActivity {
         mFalseBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this,R.string.false_pressed_toast,Toast.LENGTH_SHORT).show();
+                boolean isRight = checkAnswer(false);
+                if(isRight)
+                    Toast.makeText(QuizActivity.this,R.string.true_pressed_toast,Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(QuizActivity.this,R.string.false_pressed_toast,Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -70,6 +86,67 @@ public class QuizActivity extends ActionBarActivity {
 
             }
         });
+
+        // Set Listener on TextView to get new question
+        mQuestionView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fetchNewQuestion(mCurrentQuestionIndex);
+            }
+        });
+
+        // Set On click listener on the previous question
+        mPrevQtnBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fetchLastQuestion(mCurrentQuestionIndex);
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG,"onStart() called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG,"onResume() called");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG,"onStop() called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"onDestroy() called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG,"onPause() called");
+    }
+
+    private void fetchLastQuestion(int mCurrentQuestionIndex) {
+        if(mCurrentQuestionIndex > 0)
+        {
+            int questionId = mQuestionBankList.get(mCurrentQuestionIndex-1).getQuestion();
+            // Setting the previous question on Question View text box
+            mQuestionView.setText(questionId);
+            //Decreasing the global question index
+            this.mCurrentQuestionIndex = mCurrentQuestionIndex - 1;
+        }
+        else{
+            Toast.makeText(this,R.string.first_questn,Toast.LENGTH_SHORT).show();
+        }
+
     }
 
     private void fetchNewQuestion(int mCurrentQuestionIndex) {
@@ -80,11 +157,16 @@ public class QuizActivity extends ActionBarActivity {
             int questionId = mQuestionBankList.get(this.mCurrentQuestionIndex).getQuestion();
             // Setting the TextView with updated question
             mQuestionView.setText(questionId);
+        }else{
+            Toast.makeText(this,R.string.last_questn,Toast.LENGTH_SHORT).show();
         }
     }
 
     private boolean checkAnswer(boolean a) {
 
+        boolean answer = mQuestionBankList.get(this.mCurrentQuestionIndex).isQuestionAnswer();
+        if(a == answer)
+            return true;
         return false;
     }
 
